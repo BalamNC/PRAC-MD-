3d20fb481b2ec308b6127a7f7a2d648662fdf319
BalamNC <nietocasasbalamalain@gmail.com>
2026-02-10 08:51:42 -0600

    Add files via upload

---
+++ PRACTICA 2 MD.py
@@ -0,0 +1,55 @@
+#par o impar
+
+numero = 7
+
+if numero % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
+#positivo o negativo
+numero = -3
+
+if numero >= 0:
+    print("Positivo")
+else:
+    print("Negativo")
+#mayor y menor
+edad = 20
+
+if edad >= 18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
+#aprobado no aprobaddo
+
+calificacion = 75
+
+if calificacion >= 60:
+    print("Aprobado")
+else:
+    print("Reprobado")
+
+#calificacion letras
+calificacion = 85
+
+if calificacion >= 90:
+    print("A")
+elif calificacion >= 80:
+    print("B")
+elif calificacion >= 70:
+    print("C")
+elif calificacion >= 60:
+    print("D")
+else:
+    print("F")
+
+#temperatura
+temperatura = 25
+
+if temperatura < 0:
+    print("Sólido")
+elif temperatura <= 100:
+    print("Líquido")
+else:
+    print("Vapor")
+
